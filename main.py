199f44f6ac36cbb40ae0983f42c756161430c3c4
Maksim Marychev <marycheff17@gmail.com>
2024-09-19 23:53:12 +0400

    Добавлен код для задачи 6

---
+++ main.py
@@ -0,0 +1,32 @@
+def check_same_sign(arr):
+    if len(arr) == 0:
+        return False
+    first_sign = -1
+    for x in arr:
+        if x == 0:
+            return False
+        
+        if first_sign == -1:
+            first_sign = x > 0
+        elif (x > 0) != first_sign:
+            return False
+    return True
+
+arrays = [
+    [1.2, 3.4, 5.6, 7.8],
+    [-1.1, -2.2, -3.3],
+    [0.0, 1.1, -1.1],
+    [4.4, 5.5, 6.6],
+    [-7.7, -8.8, -9.9]
+]
+
+found = False
+i = 1
+for arr in arrays:
+    if check_same_sign(arr):
+        print(f"Массив {i} состоит из элементов одного знака.")
+        found = True
+    i += 1
+
+if not found:
+    print("Нет массивов, состоящих из элементов одного знака.")
