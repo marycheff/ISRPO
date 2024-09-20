ddd4d4ae1a8f4fb79a6acd50d69f0b904690ff55
TimZet156 <137096182+TimZet156@users.noreply.github.com>
2024-09-20 14:38:15 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
 def check_same_sign(arr):
     if len(arr) == 0:
         return False
-    first_sign = -1
+    first_sign = None
     for x in arr:
         if x == 0:
             return False
         
-        if first_sign == -1:
+        if first_sign == None:
             first_sign = x > 0
         elif (x > 0) != first_sign:
             return False
@@ -22,13 +22,14 @@ def process_arrays(arrays):
         i += 1
     if not found:
         print("Нет массивов, состоящих из элементов одного знака.")
-
-arrays = [
-    [1.2, 3.4, 5.6, 7.8],
-    [-1.1, -2.2, -3.3],
-    [0.0, 1.1, -1.1],
-    [4.4, 5.5, 6.6],
-    [-7.7, -8.8, -9.9]
-]
-
-process_arrays(arrays)
+def main():
+    arrays = [
+        [1.2, 3.4, 5.6, 7.8],
+        [-1.1, -2.2, -3.3],
+        [0.0, 1.1, -1.1],
+        [4.4, 5.5, 6.6],
+        [-7.7, -8.8, -9.9]
+    ]
+    
+    process_arrays(arrays)
+main()
